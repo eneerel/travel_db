9edf1cf5e9fd626123506c8b0561100ba42d75e2
Evening <evening@mstars-lab3-07s-iMac.local>
2023-03-06 17:38:07 +0800

    123

--- travel_db.sql
+++ travel_db.sql
@@ -1,6 +1,6 @@
--- MariaDB dump 10.19  Distrib 10.4.27-MariaDB, for Win64 (AMD64)
+-- MariaDB dump 10.19  Distrib 10.4.27-MariaDB, for osx10.10 (x86_64)
 --
--- Host: localhost    Database: travel_db
+-- Host: localhost    Database: azure_db
 -- ------------------------------------------------------
 -- Server version	10.4.27-MariaDB
 
@@ -16,29 +16,112 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `categories`
+-- Table structure for table `azure_user`
 --
 
-DROP TABLE IF EXISTS `categories`;
+DROP TABLE IF EXISTS `azure_user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categories` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(30) DEFAULT NULL,
-  `images` text DEFAULT NULL,
-  `detail` text DEFAULT NULL,
-  `price` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE `azure_user` (
+  `aid` int(11) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `owog` varchar(20) DEFAULT NULL,
+  UNIQUE KEY `aid` (`aid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `categories`
+-- Dumping data for table `azure_user`
 --
 
-LOCK TABLES `categories` WRITE;
-/*!40000 ALTER TABLE `categories` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categories` ENABLE KEYS */;
+LOCK TABLES `azure_user` WRITE;
+/*!40000 ALTER TABLE `azure_user` DISABLE KEYS */;
+INSERT INTO `azure_user` VALUES (2,'Naraa',''),(3,'jaja',NULL),(4,'s','undefined'),(5,'Turmaa',NULL),(6,'Otgoo','Lil'),(7,'Jujaaa','Tami'),(8,'Aami','Taami'),(9,'Burmaa','Huurhun'),(10,'Jims','Tsetseg');
+/*!40000 ALTER TABLE `azure_user` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `category` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `image` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `image` (`image`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Beaches','@url','All categories,travles'),(2,'Deserts','@url2','beaches details'),(3,'Mountains','@url3','Deserts details');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `rating`
+--
+
+DROP TABLE IF EXISTS `rating`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `rating` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) DEFAULT NULL,
+  `category_id` int(11) DEFAULT NULL,
+  `travel_id` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `rating`
+--
+
+LOCK TABLES `rating` WRITE;
+/*!40000 ALTER TABLE `rating` DISABLE KEYS */;
+INSERT INTO `rating` VALUES (1,1,1,1,8),(3,2,1,1,9),(4,2,2,NULL,9),(5,3,2,NULL,9),(6,3,1,1,100);
+/*!40000 ALTER TABLE `rating` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `travel`
+--
+
+DROP TABLE IF EXISTS `travel`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `travel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `images` text DEFAULT NULL,
+  `detail` text DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `location` text DEFAULT NULL,
+  `day` int(11) DEFAULT NULL,
+  `cat_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `travel`
+--
+
+LOCK TABLES `travel` WRITE;
+/*!40000 ALTER TABLE `travel` DISABLE KEYS */;
+INSERT INTO `travel` VALUES (1,'Mauritius','./images/image 27.png','Tour combo with return airport transter',88,'url',7,1),(2,'Jeju','./images/image 27.png','Tour combo with return airport transter',300,'url',7,2),(3,'Japan Puji_Mountain','./images/image 27.png','Tour combo with return airport transter',300,'url',7,3);
+/*!40000 ALTER TABLE `travel` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -55,7 +138,7 @@ CREATE TABLE `users` (
   `email` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -64,6 +147,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Naraa','pass123','naraa@gmail.com'),(2,'Saraa','pass123','saraa@gmail.com'),(3,'Baraa','pass123','baraa@gmail.com');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -76,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-03-06 13:55:01
+-- Dump completed on 2023-03-06 17:37:50
